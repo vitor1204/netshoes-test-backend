545af3044dfd7081063f6d4d2a43570ae471e8c8
vitor1204 <37661816+vitor1204@users.noreply.github.com>
2018-04-07 13:06:24 -0300

    alterações de teste
    Alterações do teste

--- src/main/java/com/netshoes/problems/problem1/impl/SearchVowel.java
+++ src/main/java/com/netshoes/problems/problem1/impl/SearchVowel.java
@@ -6,11 +6,54 @@ import com.netshoes.problems.problem1.api.Stream;
 public class SearchVowel {
 
     // TODO sua implementacao! Fique a vontade para criar suas classes/interfaces caso julgue necessario.
-
+	public static void main(String[] args) {
+	
+		StreamImpl stream = new StreamImpl("aAbBABacafe ", 0);	
+		firstChar(stream);
+	}
+	
     // NAO ALTERAR ESSA ASSINATURA!
     public static char firstChar(Stream input) {
         // TODO sua implementacao! Nesse ponto deve ser feita a adaptacoes necessarias para que os testes executem com sucesso
-        return 'x';
+        
+    	char x = '0';
+    	char atual = '0';
+    	int i = 0;
+    	boolean y = false;
+    	StreamImpl castInput = (StreamImpl) input;
+    	String streamWord =  castInput.getStream().replaceAll(" ",""); //poderia usar o trim também, mas como quero usar a palavra sem espaços em branco
+    	char[] caracters = new char[streamWord.length()];
+    	
+    	
+    	while(castInput.hasNext() && i < streamWord.length()) {		
+    		
+    		atual = castInput.getNext();
+    		
+    		if (atual == 'A'  ||  atual == 'E' || atual == 'I' || atual == 'O' || atual == 'U' || atual == 'a'  ||
+    				atual == 'e' || atual == 'i' || atual == 'o' || atual == 'u') {
+    			
+	    		for(int i2 = 0; i2 < caracters.length; i2++) {
+	    			if (atual == caracters[i2]) {
+	    				y = true;	
+	    			}
+	    		}		
+	    		
+	    		if(y == false)
+	    			 x = atual;
+	
+	    		caracters[i] = atual;
+	    		
+	    		i++;
+	    		castInput.setPosition(i);
+	    		y = false;
+    		}
+    		
+    	}
+    	
+    	System.out.println(x);
+    	
+    	return x;
     }
+    
 
 }
--- src/main/java/com/netshoes/problems/problem1/impl/StreamImpl.java
+++ src/main/java/com/netshoes/problems/problem1/impl/StreamImpl.java
@@ -5,23 +5,56 @@ import com.netshoes.problems.problem1.api.Stream;
 
 // NAO RENOMEAR ESSA CLASSE!
 public class StreamImpl implements Stream {
-
-    // TODO sua implementacao! Fique a vontade para criar suas classes/interfaces caso julgue necessario.
-
-    // Nao alterar esse construtor!
-    public StreamImpl(String stream) {
+	
+	// TODO sua implementacao! Fique a vontade para criar suas classes/interfaces caso julgue necessario.
+	private String stream;
+	private int position;
+	
+	// Nao alterar esse construtor!
+    public StreamImpl(String stream) { 
+    	this.stream = stream;
+    	
+        // TODO sua implementacao! Fique a vontade para criar suas classes/interfaces caso julgue necessario.
+    }
+	
+    public StreamImpl(String stream, int position) { 
+    	this.stream = stream.toUpperCase();
+    	this.position = position;
+    	
         // TODO sua implementacao! Fique a vontade para criar suas classes/interfaces caso julgue necessario.
     }
 
-    @Override
+    public String getStream() {
+		return stream.toUpperCase();
+	}
+
+	public void setStream(String stream) {
+		this.stream = stream;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void setPosition(int position) {
+		this.position = position;
+	}
+
+	@Override
     public char getNext() {
-        // TODO sua implementacao! Fique a vontade para criar suas classes/interfaces caso julgue necessario.
-        return 0;
+        // TODO sua implementacao! Fique a vontade para criar suas classes/interfaces caso julgue necessario.		
+        return stream.charAt(this.position);
     }
 
     @Override
     public boolean hasNext() {
         // TODO sua implementacao! Fique a vontade para criar suas classes/interfaces caso julgue necessario.
-        return false;
+    	boolean ret = true;
+    	
+    	if( (this.position + 1) > this.stream.length()) {
+    		ret = false;
+    	}
+    	
+        return ret;
     }
 }
